681314b11aec9cf8cc19a79b5104a9be464f3e41
Grant Orchard <go@hashicorp.com>
2020-04-02 20:42:41 +1100

    updating for github

--- main.tf
+++ main.tf
@@ -1,18 +1,23 @@
-resource bitbucketserver_repository "this" {
-  project     = var.repository_project_key
-  name        = var.repository_name
-  description = var.repository_description
-  public      = var.repository_is_public
-  slug        = var.repository_slug
-}
-
 resource tfe_workspace "this" {
   name         = var.tfe_workspace_name
   organization = var.tfe_org_name
   auto_apply   = var.tfe_auto_apply
+
   vcs_repo {
-    identifier = "${var.repository_project_key}/${bitbucketserver_repository.this.slug}"
+    identifier = github_repository.this.full_name
     branch     = var.repository_branch
     oauth_token_id = var.oauth_token_id
+  }
+}
+
+resource github_repository "this" {
+  name        = var.repository_name
+  description = var.repository_description
+
+  private = var.repository_private
+
+  template {
+    owner = var.template_repository_owner
+    repository = var.template_repository_name
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,50 +1,46 @@
-variable repository_owner {
+variable template_repository_owner {
   type = string
-  description = "The name of the team who will own the newly created BitBucket repository."
+  default = "grantorchard"
 }
+
+variable template_repository_name {
+  type = string
+  default = "terraform-repository-template"
+}
+
 variable repository_name {
   type = string
-  description = "Display name of the repository to be created and used by the new Terraform workspace."
 }
-variable repository_slug {
-  type = string
-  description = "The name of the repository in the context of the URL. Typically in the form of the repository display name, but replacing spaces with dashes."
-}
-variable repository_project_key {
-  type = string
-  description = "The name of the BitBucket project under which to create this repository."
-  default = "DEMOS"
-}
+
 variable repository_description {
   type = string
-  description = "Adds a description to both the newly created BitBucket repository and Terraform Enterprise workspace."
-  default = "Created by Terraform Enterprise."
+  default = ""
 }
-variable repository_is_public {
-  type = bool
-  description = "Whether the repository should be public."
-  default = true
-}
-variable tfe_workspace_name {
-  type = string
-  description = "The name of the Terraform Enterprise Workspace. Cannot contain spaces."
-}
-variable tfe_org_name {
-  type = string
-  description = "The Terraform Enterprise organisation to create the workspace within."
-}
-variable tfe_auto_apply {
-  type = string
-  description = "Determines whether manual intervention is required to move from 'plan' to 'apply'."
-  default = "True"
 
-}
 variable repository_branch {
   type = string
-  description = "Set the branch to monitor for changes that will trigger a Terraform run."
   default = "master"
 }
+
 variable oauth_token_id {
   type = string
-  description = "The oath token to use for registering the Bitbucket repository with the Terraform Enterprise workspace. This value can be retrieved from the VCS Providers configuration page in the Settings menu for your Organization."
+}
+
+variable tfe_org_name {
+  type = string
+  default = "grantorchard"
+}
+
+variable tfe_workspace_name {
+  type = string
+}
+
+variable tfe_auto_apply {
+  type = bool
+  default = true
+}
+
+variable repository_private {
+  type = bool
+  default = false
 }
